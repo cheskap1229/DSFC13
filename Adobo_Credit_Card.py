da1ffcac94455287567a1d9692c616e812ef682d
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 02:42:32 +0800

    Update Adobo_Credit_Card.py

--- Adobo_Credit_Card.py
+++ Adobo_Credit_Card.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.image('images/adobo.png', width=100)
-st.title ('HELLO, :red[RISKS] , GOODBYE') 
+st.title ('HELLO, :red[RISKS] , GOODBYE.') 
 st.header('Profiling Churn Risk of Customers for Cost Optimization', divider='red')
 
 st.subheader("The Challenge:")
@@ -10,4 +10,10 @@ st.subheader("The Goal:")
 st.markdown("Develop sales and marketing strategies that will further increase customers' card usage.")
 st.markdown("---")
 
-st.caption("this is the caption")
+st.subheader("Risk-Profiling Objectives:")
+st.markdown("In classifying the clients of Adobo Credit Card according to their churn risk, valuable insights on the clients can be obtained based on the following areas.")
+
+st.markdown("Spending Behavior:")
+st.caption("Spending frequency, Seasonal spending trends and patterns, Categorical budget allocation, Payment mode preferences")
+st.markdown("Customer Demographic:")
+st.caption("Customer age groups, Geographical location, Gender, Industry of work, profession")
