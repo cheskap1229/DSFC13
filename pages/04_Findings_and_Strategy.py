97253611b44efc34fbe2461a9dce2d94b4fcf8f3
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 07:50:46 +0800

    Update 04_Findings_and_Strategy.py

--- pages/04_Findings_and_Strategy.py
+++ pages/04_Findings_and_Strategy.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-st.title("Summary")
-st.write("Here you can provide a short summary and recommendations")
+st.title("Recommendations and Strategy")
+st.header("Here you can provide a short summary and recommendations")
+
+st.image('reco1.png')
+st.caption("Limitations")
+st.markdown("---")
+
+st.image('reco2.png')
+st.caption("Limitations")
+st.markdown("---")
+
+st.image('reco3.png')
+st.caption("Limitations")
+st.markdown("---")
+
+st.image('reco4.png')
+st.caption("Limitations")
+st.markdown("---")
