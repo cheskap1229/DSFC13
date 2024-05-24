42e734e34ae2def21040c39e2669773c3da42f4b
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 02:08:50 +0800

    Update and rename test.py to Adobo_Credit_Card.py

---
+++ Adobo_Credit_Card.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title ('HELLO, RISKS, GOODBYE')
+st.header('Profiling At-Risk Customers for Cost-Optimization', divider='red')
+
+st.subheader("The Challenge:")
+st.markdown("Adobo Credit Card strives to optimize sales, boost business performance, and drive customer satisfaction.")
+st.subheader("The Goal:")
+st.markdown("Develop sales and marketing strategies that will further increase customers' card usage.")
+
+st.caption("this is the caption")
