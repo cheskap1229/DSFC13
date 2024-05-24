f248ebb4d5bbd2f102290145c2de1ef041f239ad
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 03:06:35 +0800

    Update Adobo_Credit_Card.py

--- Adobo_Credit_Card.py
+++ Adobo_Credit_Card.py
@@ -5,7 +5,7 @@ st.title ('HELLO, :red[RISKS] , GOODBYE.')
 st.header('Profiling Churn Risk of Customers for Cost Optimization', divider='red')
 
 st.subheader("The Challenge:")
-st.markdown("Adobo Credit Card strives to optimize sales, boost business performance, and drive customer satisfaction.")
+st.markdown("Adobo Credit Card (ACC) strives to optimize sales, boost business performance, and drive customer satisfaction.")
 st.subheader("The Goal:")
 st.markdown("Develop sales and marketing strategies that will further increase customers' card usage.")
 st.markdown("---")
@@ -20,4 +20,7 @@ st.caption("Customer age groups, Geographical location, Gender, Industry of work
 st.markdown("---")
 
 st.image('adobo_clients.png')
+st.caption("Based on 100,000 transaction data points obtained from January 2020 to December 2021, ACC was observed to have 94 unique clients. The majority of these customers are male, making up 93.6%, and range from 51-95 years old. They live in 172 cities across the Philippines, with the greatest percentage in Luzon, followed by Mindanao then Visayas.")
+st.markdown("---")
 
+#st.image('methodology.png')
