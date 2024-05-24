0a7f38517cf12806814aee005dc1c3bb3817b215
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 07:46:36 +0800

    Update 02_KMeans_Clustering.py

--- pages/02_KMeans_Clustering.py
+++ pages/02_KMeans_Clustering.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-st.title("Results")
-st.write("Here you can put each of your key results.")
+st.title("K-Means Clustering")
+st.header("Here you can put each of your key results.")
+
+st.image('kmeans2.png')
+st.caption("K-means")
+st.markdown("---")
+
+st.image('kmeans.png')
+st.caption("K-means")
+st.markdown("---")
