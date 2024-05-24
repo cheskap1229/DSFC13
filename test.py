b8bbcd485f83eed8e5e85f3a04889f28ee9e819f
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 01:40:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("Hello, let's learn how to build a streamlit app together")
+
+st.title ('''
+  :white[HELLO,] red:[RISKS] white:[GOODBYE]''')
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
