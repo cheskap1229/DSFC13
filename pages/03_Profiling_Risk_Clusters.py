92b09557ae20999ab4dd1ca2c7ae335101d81652
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 07:49:17 +0800

    Update 03_Profiling_Risk_Clusters.py

--- pages/03_Profiling_Risk_Clusters.py
+++ pages/03_Profiling_Risk_Clusters.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-st.title("Results")
-st.write("Here you can put each of your key results.")
+st.title("Profiling Risk Clusters")
+st.header("Here you can put each of your key results.")
+
+st.image('purchasing1.png')
+st.caption("Limitations")
+st.markdown("---")
+
+st.image('purchasing2.png')
+st.caption("Limitations")
+st.markdown("---")
+
+st.image('purchasing3.png')
+st.caption("Limitations")
+st.markdown("---")
+
+st.image('transaction_volume.png')
+st.caption("Limitations")
+st.markdown("---")
