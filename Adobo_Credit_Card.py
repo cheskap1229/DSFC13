5d4a20b95792d4de9a3d06ccbdeabe99d4e5e19f
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 06:34:03 +0800

    Update Adobo_Credit_Card.py

--- Adobo_Credit_Card.py
+++ Adobo_Credit_Card.py
@@ -19,8 +19,12 @@ st.markdown("Customer Demographic:")
 st.caption("Customer age groups, Geographical location, Gender, Industry of work, profession")
 st.markdown("---")
 
-st.image('adobo_clients.png')
-st.caption("Based on 100,000 transaction data points obtained from January 2020 to December 2021, ACC was observed to have 94 unique clients. The majority of these customers are male, making up 93.6%, and range from 51-95 years old. They live in 172 cities across the Philippines, with the greatest percentage in Luzon, followed by Mindanao then Visayas.")
+st.image('methodology.png')
+st.caption("These are the five components of the methodology. Problem Scoping create quantifiable goals and objectives.")
 st.markdown("---")
 
-#st.image('methodology.png')
+st.image('adobo_clients.png')
+st.caption("Who are the Adobo clients? Based on 100,000 transaction data points obtained from January 2020 to December 2021, ACC was observed to have 94 unique clients. The majority of these customers are male, making up 93.6%, and range from 51-95 years old. They live in 172 cities across the Philippines, with the greatest percentage in Luzon, followed by Mindanao then Visayas.")
+st.markdown("---")
+
+
