085fdb2a236372df1d699551d876b87e916f214f
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 01:55:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 st.title ('HELLO, RISKS, GOODBYE')
 st.header('this is the markdown', divider='red')
 st.markdown("this is the header")
