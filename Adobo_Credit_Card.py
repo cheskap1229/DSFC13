c5f78d6749f5926e8096a3e615264fcb49ecce5a
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 02:14:15 +0800

    Update Adobo_Credit_Card.py

--- Adobo_Credit_Card.py
+++ Adobo_Credit_Card.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title ('HELLO, RISKS, GOODBYE') st.image('adobo.png')
+st.title ('HELLO, RISKS, GOODBYE')
+st.image('adobo.png')
 st.header('Profiling At-Risk Customers for Cost-Optimization', divider='red')
 
 st.subheader("The Challenge:")
