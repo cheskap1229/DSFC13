acc21e59c16bbef478a767f4316050835a6097d9
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 01:52:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title ('HELLO, RISKS,GOODBYE', divider='red')
-st.header("this is the markdown")
+st.title ('HELLO, RISKS, GOODBYE')
+st.header("this is the markdown, divider='red')
 st.markdown("this is the header")
 st.subheader("this is the subheader")
 st.caption("this is the caption")
