5cf6f13f791d67e41fac105b6539c1400f3f7a95
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 02:29:31 +0800

    Update Adobo_Credit_Card.py

--- Adobo_Credit_Card.py
+++ Adobo_Credit_Card.py
@@ -7,6 +7,7 @@ st.header('Profiling Churn Risk of Customers for Cost Optimization', divider='re
 st.subheader("The Challenge:")
 st.markdown("Adobo Credit Card strives to optimize sales, boost business performance, and drive customer satisfaction.")
 st.subheader("The Goal:")
-st.markdown("Develop sales and marketing strategies that will further increase customers' card usage.", divider='red')
+st.markdown("Develop sales and marketing strategies that will further increase customers' card usage.")
+st.markdown("---")
 
 st.caption("this is the caption")
