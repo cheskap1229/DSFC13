0a3cb1835de1d88937cd2003d8d5744e6cc7532f
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 11:26:41 +0800

    Update 04_Findings_and_Strategy.py

--- pages/04_Findings_and_Strategy.py
+++ pages/04_Findings_and_Strategy.py
@@ -3,17 +3,13 @@ import streamlit as st
 st.title("Recommendations and Strategy")
 
 st.image('reco1.png')
-st.caption("Limitations")
 st.markdown("---")
 
 st.image('reco2.png')
-st.caption("Limitations")
 st.markdown("---")
 
 st.image('reco3.png')
-st.caption("Limitations")
 st.markdown("---")
 
 st.image('reco4.png')
-st.caption("Limitations")
 st.markdown("---")
