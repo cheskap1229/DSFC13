b7c938fa2ac6061e58a1d131f17c304fb0c4a28d
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 11:06:26 +0800

    Update 03_Profiling_Risk_Clusters.py

--- pages/03_Profiling_Risk_Clusters.py
+++ pages/03_Profiling_Risk_Clusters.py
@@ -1,20 +1,24 @@
 import streamlit as st
 
 st.title("Profiling Risk Clusters")
-st.header("Here you can put each of your key results.")
+st.header("Here you can put each of your key results.", divider = 'red')
 
+st.subheader('Average Inactivity')
 st.image('purchasing1.png')
 st.caption("Limitations")
 st.markdown("---")
 
+st.subheader('Average Spending (PHP) per Transaction')
 st.image('purchasing2.png')
 st.caption("Limitations")
 st.markdown("---")
 
+st.subheader('One-time Purchaser')
 st.image('purchasing3.png')
 st.caption("Limitations")
 st.markdown("---")
 
+st.subheader('Trend of Transaction Volume')
 st.image('transaction_volume.png')
 st.caption("Limitations")
 st.markdown("---")
