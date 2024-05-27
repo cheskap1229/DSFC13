229ffba232f208cc368507446536141a038b25f1
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-27 15:57:36 +0800

    Update Adobo_Credit_Card.py

--- Adobo_Credit_Card.py
+++ Adobo_Credit_Card.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.image('images/adobo.png', width=100)
 st.title ('HELLO, :red[RISKS], GOODBYE.') 
 st.header('Profiling Churn Risk of Customers for Cost Optimization', divider='red')
+st.caption('Group 1 Exp1orers - Francesca Panga | Jacob Fuentebella | Jersy | Leonard Inciso | Lester Covarrubias II')
 
 st.subheader("The Challenge:")
 st.markdown("Adobo Credit Card (ACC) strives to optimize sales, boost business performance, and drive customer satisfaction.")
